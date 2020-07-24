6ce47ed4389b0d23337731c91300ea605c5546c1
gmftbyGMFTBY <18811371908@163.com>
2020-07-24 02:03:17 +0000

    Fri Jul 24 02:03:17 UTC 2020 add the backup script

---
+++ data/backup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# only backup the text file
+rm -rf backup
+mkdir -p backup
+folder=(zh50w zhihu MTDialogRewritten ECG zhidao kgdialog kdconv doubangroup xiaohuangji weibo400w ptt qingyun11w douban300w)
+
+for path in ${folder[@]}
+do
+    echo "[!] backup $path folder"
+    mkdir -p backup/$path
+    # copy file
+    if [ $path = 'zh50w' ]; then
+        cp $path/train.txt backup/$path/
+        cp $path/train_.txt backup/$path/
+        cp $path/test.txt backup/$path/
+    elif [ $path = 'zhihu' ]; then
+        cp $path/train.txt backup/$path/
+        cp $path/train_.txt backup/$path/
+    elif [ $path = 'MTDialogRewritten' ]; then
+        cp $path/train.txt backup/$path/
+        cp $path/corpus.txt backup/$path
+    elif [ $path = 'ECG' ]; then
+        cp $path/train.txt backup/$path
+        cp $path/ecg_train_data.json backup/$path
+        cp $path/ecg_test_data.xlsx backup/$path
+    elif [ $path = 'zhidao' ]; then
+        cp $path/train.txt backup/$path
+        cp $path/filter_zhidao.txt backup/$path
+    elif [ $path = 'kgdialog' ]; then
+        cp $path/dev.txt backup/$path
+        cp $path/test.txt backup/$path
+        cp $path/train.txt backup/$path
+        cp $path/train_.txt backup/$path
+    elif [ $path = 'kdconv' ]; then
+        cp $path/*.json backup/$path
+        cp $path/train.txt backup/$path
+    elif [ $path = 'doubangroup' ]; then
+        cp $path/train.txt backup/$path
+    elif [ $path = 'xiaohuangji' ]; then
+        cp $path/test.txt backup/$path
+        cp $path/train.txt backup/$path
+    elif [ $path = 'weibo400w' ]; then
+        cp $path/stc* backup/$path
+        cp $path/train.txt backup/$path
+    elif [ $path = 'ptt' ]; then
+        cp $path/ptt.txt backup/$path
+        cp $path/train.txt backup/$path
+    elif [ $path = 'qingyun11w' ]; then
+        cp $path/train.txt backup/$path
+        cp $path/qingyun.csv backup/$path
+    elif [ $path = 'douban300w' ]; then
+        cp $path/dev.txt backup/$path
+        cp $path/test.txt backup/$path
+        cp $path/train.txt backup/$path
+        cp $path/train_.txt backup/$path
+    else
+        echo "[!] got unknow dataset $path"
+    fi
+done
